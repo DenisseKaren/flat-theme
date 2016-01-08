1f65d93d6a10043568b7b3c4051930733c593471
denisse <daniela_1703_91@hotmail.com>
2016-01-08 03:19:51 -0500

    Este es el proyecto flat-theme

---
+++ js/main.js
@@ -0,0 +1,36 @@
+jQuery(function ($) {
+
+    $('.centered').each(function (e) {
+        $(this).css('margin-top', ($('#main-slider').height() - $(this).height()) / 2);
+    });
+
+    $(window).resize(function () {
+        $('.centered').each(function (e) {
+            $(this).css('margin-top', ($('#main-slider').height() - $(this).height()) / 2);
+        });
+    });
+
+
+    //contact form
+    var form = $('.contact-form');
+    form.submit(function () {
+        $this = $(this);
+        $.post($(this).attr('action'), function (data) {
+            $this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
+        }, 'json');
+        return false;
+    });
+
+    //goto top
+    
+    
+    $('.gototop').click(function (event) {
+        TweenMax.to('body',4 {'scrollTo':'(y=0)', ease:Power4, easeOut});
+        
+        /*
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("body").offset().top
+        }, 500);
+    });
+});
